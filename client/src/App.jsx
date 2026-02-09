3468432d45146402cc2ee49bb989ac1d6d2ad4d1
PraisesPJMT <praisesmusa@outlook.com>
2026-02-09 15:12:19 +0100

    chore: Add authentication routes to router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
 import { createBrowserRouter } from "react-router";
 
 import Home from "./pages/Home";
+import Signin from "./pages/auth/Signin";
+import Signup from "./pages/auth/Signup";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import SupervisorOnboarding from "./pages/auth/SupervisorOnboarding";
 
 export const interhubApp = createBrowserRouter([
   {
     path: "/",
     Component: Home,
+  },
+  {
+    path: "/signin",
+    Component: Signin,
+  },
+  {
+    path: "/signup",
+    Component: Signup,
+  },
+  {
+    path: "/forgot-password",
+    Component: ForgotPassword,
+  },
+  {
+    path: "/auth/supervisor-onboarding",
+    Component: SupervisorOnboarding,
   },
 ]);
