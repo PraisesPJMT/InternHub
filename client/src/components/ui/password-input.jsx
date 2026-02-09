8ae1d8aaeaeb0a2d1cf7953251091a7a075305e2
PraisesPJMT <praisesmusa@outlook.com>
2026-02-09 15:08:30 +0100

    feat: Add UI components and input primitives

---
+++ client/src/components/ui/password-input.jsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from "./input-group";
+
+const PasswordInput = ({ className, ...props }) => {
+  const [show, setShow] = React.useState(false);
+
+  const handleToggle = () => setShow(!show);
+
+  return (
+    <InputGroup>
+      <InputGroupInput
+        type={show ? "text" : "password"}
+        className={cn(className)}
+        {...props}
+      />
+      <InputGroupAddon align="inline-end">
+        <InputGroupButton size="icon-xs" onClick={handleToggle}>
+          {show ? <EyeOffIcon /> : <EyeIcon />}
+        </InputGroupButton>
+      </InputGroupAddon>
+    </InputGroup>
+  );
+};
+
+export { PasswordInput };
