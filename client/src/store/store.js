f1350412d271805d7081822fc1bfbeb9c5326a21
PraisesPJMT <praisesmusa@outlook.com>
2026-02-10 17:59:59 +0100

    feat: Enable persistence and logging for Redux store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,40 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import logger from 'redux-logger'
+
 import { authSlice } from './slice/auth/authSlice'
 
-export const store = configureStore({
-  reducer: {
-    authStore: authSlice.reducer,
-  },
+const rootReducer = combineReducers({
+  authStore: authSlice.reducer,
 })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['authStore'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const isDev = import.meta.env.DEV
+
+export const store = configureStore({
+  reducer: persistedReducer,
+
+  middleware: (getDefaultMiddleware) => {
+    const middleware = getDefaultMiddleware({
+      serializableCheck: false,
+    })
+
+    if (isDev) {
+      middleware.push(logger)
+    }
+
+    return middleware
+  },
+
+  devTools: isDev,
+})
+
+export const persistor = persistStore(store)
